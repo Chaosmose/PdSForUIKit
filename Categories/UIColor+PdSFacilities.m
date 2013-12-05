b63be05f97427334fe49ad19c6bf74650a43e664
Benoit PEREIRA DA SILVA <benoit@pereira-da-silva.com>
2013-12-05 15:16:22 +0100

    UIColor PdSFacilities

---
+++ Categories/UIColor+PdSFacilities.m
@@ -0,0 +1,54 @@
+// This file is part of "PDSCategoriesForUIKit"
+//
+// "PDSCategoriesForUIKit" is free software: you can redistribute it and/or modify
+// it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// "PdSMatrix" is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU LESSER GENERAL PUBLIC LICENSE for more details.
+//
+// You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+// along with "PDSCategoriesForUIKit"  If not, see <http://www.gnu.org/licenses/>
+//
+//  Created by Benoit Pereira da Silva on 05/12/2013.
+//
+//
+
+#import "UIColor+PdSFacilities.h"
+
+@implementation UIColor (PdSFacilities)
+
+/**
+ *  A ligther color
+ *
+ *  @return the color
+ */
+- (UIColor*)lighter{
+    CGFloat h, s, b, a;
+    if ([self getHue:&h saturation:&s brightness:&b alpha:&a])
+        return [UIColor colorWithHue:h
+                          saturation:s
+                          brightness:MIN(b * 1.25, 1.0)
+                               alpha:a];
+    return nil;
+}
+
+/**
+ *  A darker color
+ *
+ *  @return the color
+ */
+- (UIColor*)darker{
+    CGFloat h, s, b, a;
+    if ([self getHue:&h saturation:&s brightness:&b alpha:&a])
+        return [UIColor colorWithHue:h
+                          saturation:s
+                          brightness:b * 0.75
+                               alpha:a];
+    return nil;
+}
+
+@end
